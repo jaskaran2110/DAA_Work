5be94e2b1a778385337a802637c7eb89d0d45c43
Jaskaran Singh <57746744+jaskaran2110@users.noreply.github.com>
2021-09-23 02:42:43 +0530

    Update and rename linear search to linear search.cpp

---
+++ Week1/linear search.cpp
@@ -0,0 +1,43 @@
+/*
+ * Given an array of nonnegative integers, design a linear algorithm and
+ * implement it using a program to find whether given key element is persent in
+ * the array or not. Also find the total number of comparisions for each input
+ * case. Time complexity = O(n), where n is the size of the input
+ */
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int key;
+        cin >> key;
+        int flag = 0,counter=0;
+        for (int i = 0; i < n; i++) {
+            counter ++;
+            if (arr[i] == key) {
+                flag=1;
+                break;
+            }
+        }
+        if(flag ==1) {
+            cout << "Present " << counter << endl;
+            
+        }
+        else {
+            cout << "Not Present " << counter << endl;
+        }
+        
+    }
+    return 0;
+}
