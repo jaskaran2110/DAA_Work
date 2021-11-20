e183faab7785757d7ae8641b5aa50f9123865ce6
Jaskaran Singh <jaskaranwinchester@gmail.com>
2021-11-20 19:26:31 +0530

    Update Question3.cpp

--- Week2/Question3/Question3.cpp
+++ Week2/Question3/Question3.cpp
@@ -1,27 +1,30 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
-    int n,sum;
-    cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    cin>>sum;
-    int count=0;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n,sum;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
         {
-            if(a[i]+a[j]==sum)
+            cin>>a[i];
+        }
+        cin>>sum;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
             {
-                count++;
+                if(a[i]+a[j]==sum)
+                {
+                    count++;
+                }
             }
         }
-    }
-    cout<<count<<endl;
+        cout<<count<<endl;
+    }    
     return 0;
 }
